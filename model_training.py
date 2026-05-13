a72145af72e5af71786c6b036a717e9c80f6ad21
Siva M Reddy Annapareddy <sannapa2016@gmail.com>
2026-05-13 15:16:11 +0530

    Create model_training.py

---
+++ model_training.py
@@ -0,0 +1,5 @@
+Step A: Feature Engineering. Process variables like "Time since last refill."
+
+Step B: XGBoost Implementation. Train the model to identify patterns in discontinuation.
+
+Step C: SHAP Integration. This is critical. It generates a summary plot showing exactly why a patient is predicted to drop off.
